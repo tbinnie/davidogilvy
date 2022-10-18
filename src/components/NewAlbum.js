7b4263cdb453c71bbfe667d91b5c195b9965f459
Tom <tombinnie@protonmail.com>
2022-10-18 14:03:18 +0100

    update layout

--- src/components/NewAlbum.js
+++ src/components/NewAlbum.js
@@ -4,22 +4,19 @@ import bandcampIcon from "../imgs/icons/bandcamp-icon.svg";
 
 const NewAlbum = () => {
   return (
-    <section className="mainsite lg:w-[75%] ml-0 mr-auto bg-[url('./imgs/background-trim.png')] h-screen bg-cover bg-center p-6">
-      <div className="new_album_wrapper flex flex-col lg:flex-row justify-between gap-10 lg:gap-0 bg-dobeige lg:bg-transparent bg-opacity-70 rounded-xl">
-        <h1 className="text-5xl tracking-widest w-56 ">
-          A SCOTTISH AMERICAN SONGBOOK DAVID OGILVY
-        </h1>
-        <div className="flex flex-col gap-8 items-end justify-center">
-          <h2 className="text-4xl font-bold">
-            Listen on Spotify{" "}
-            <img src={spotifyIcon} className="h-16 w-auto inline" />
-          </h2>
-          <h2 className="text-4xl font-bold">
+    <section className="mainsite lg:w-[75%] ml-0 mr-auto bg-[url('./imgs/background-1800.png')] h-screen bg-cover p-6 relative">
+        <div className="flex flex-col gap-8 items-end justify-center absolute bottom-20 right-4 lg:bottom-8 lg:right-8">
+          <a href="https://open.spotify.com/album/7Lm1DIcgfT01ZJRqirXFTR?si=_1cRHihEQiCiv445nSY7JA" className="hover:scale-110">
+            <h2 className="text-xl lg:text-3xl font-medium">
+              Listen on Spotify{" "}
+              <img src={spotifyIcon} className=" h-8 lg:h-12 w-auto inline" />
+            </h2>
+          </a>
+          <h2 className="text-xl lg:text-3xl font-medium">
             Buy on Bandcamp{" "}
-            <img src={bandcampIcon} className="h-16 w-auto inline" />
+            <img src={bandcampIcon} className="h-8 lg:h-12 w-auto inline" />
           </h2>
         </div>
-      </div>
     </section>
   );
 };
