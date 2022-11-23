082910b758176a66f6c4e022ee125168f84decdf
Tom <tombinnie@protonmail.com>
2022-11-23 11:55:07 +0000

    edit image and build

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import heroImage from "../imgs/hero-img.jpeg"
+import heroImage from "../imgs/hero-img.png"
 
 const Landing = () => {
   return (
@@ -8,7 +8,6 @@ const Landing = () => {
           <h1 className="text-4xl tracking-[1.5rem] text-center py-12">DAVID OGILVY</h1>
           <img src={heroImage} className="lg:h-[45rem] w-auto mx-auto mb-12"/>
         </div>
-
     </section>
   );
 };
