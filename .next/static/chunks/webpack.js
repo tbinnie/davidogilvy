044967b830e87db4f9239fbc68fa9006bd1a5557
tbinnie <tom@callback.is>
2023-06-23 11:33:02 +0100

    changed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "39f976f9fac17fd1"; }
+/******/ 		__webpack_require__.h = function() { return "bde35f00ab799d30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import React from "react";
 
 const listItems = ["GIGS", "NEWSLETTER"];
-const listItems = ["GIGS", "NEWSLETTER"];
 
 export default function Header({ selected }: { selected?: string }) {
   return (
