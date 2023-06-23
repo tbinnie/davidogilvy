1256d1781f2ffb6a2239e18560096185303d538e
tbinnie <tom@callback.is>
2023-06-23 11:29:53 +0100

    add instagram

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c662b42199a8558"; }
+/******/ 		__webpack_require__.h = function() { return "39f976f9fac17fd1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
