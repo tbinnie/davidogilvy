628b5d96fb937647e1edf31d80299040e962f67e
tbinnie <tom@callback.is>
2023-06-23 11:27:19 +0100

    change instagram link and store link

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a12a7183f8913abd"; }
+/******/ 		__webpack_require__.h = function() { return "7c662b42199a8558"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,13 @@
 import Link from "next/link";
 import React from "react";
 
-const listItems = ["GIGS", "NEWSLETTER", "SOCIAL"];
+const listItems = ["GIGS", "NEWSLETTER"];
 
 export default function Header({ selected }: { selected?: string }) {
   return (
     <div className="flex justify-end absolute top-8 left-8">
       <ul className="flex text-sm lg:flex-col tracking-wider text-gray-400 gap-4 lg:gap-0">
-        <li
-          className={`${
-            !selected ? "text-black" : ""
-          } hover:text-black `}
-        >
+        <li className={`${!selected ? "text-black" : ""} hover:text-black `}>
           <Link href="/">HOME</Link>
         </li>
         <li
@@ -21,8 +17,11 @@ export default function Header({ selected }: { selected?: string }) {
         >
           <Link href="/bio">BIO</Link>
         </li>
-        <a href="https://davidogilvy.uk/" target="_blank">
+        <a href="https://davidogilvy.uk/merch" target="_blank">
           <li className="hover:text-black">STORE</li>
+        </a>
+        <a href="https://www.instagram.com/davidjogilvy/" target="_blank">
+          <li className="hover:text-black">INSTAGRAM</li>
         </a>
         {listItems.map((item) => (
           <li key={item} className="group relative">
